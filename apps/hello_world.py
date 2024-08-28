f4aae1513764ce41380e690beb6945bcacae0b38
Rolf Benschop <rolf.benschop@MONKEYPROOF.LAN>
2024-08-28 21:26:25 +0200

    reduce hello world to one working example

--- apps/hello_world.py
+++ apps/hello_world.py
@@ -1,7 +1,8 @@
 from simian.gui import Form, component, component_properties, utils
+from datetime import datetime
 
 examples_url = "https://github.com/Simian-Web-Apps/simian-deploy-render"
-hello_world_step = 1
+hello_world_step = 2
 
 # Run this file locally
 if __name__ == "__main__":
@@ -32,16 +33,13 @@ def gui_init(meta_data: dict) -> dict:
 
     # Create a textfield.
     hello_text = component.TextField("helloKey", form)
-    hello_text.label = "Enter first word"
+    hello_text.label = "Enter greeting to world"
     hello_text.defaultValue = "Hello"
 
     # Create a button.
     world_button = component.Button("buttonKey", form)
-    world_button.label = "World!"
-    world_button.disabled = True
-    world_button.tooltip = (
-        "Button disabled because no event and corresponding callback defined yet."
-    )
+    world_button.label = "Greet World!"
+    world_button.setEvent("WorldButtonPushed")
 
     # Create a textfield.
     result_text = component.TextField("resultKey", form)
@@ -53,4 +51,16 @@ def gui_init(meta_data: dict) -> dict:
 
 
 def gui_event(meta_data: dict, payload: dict) -> dict:
+    # Process the events.
+    Form.eventHandler(WorldButtonPushed=say_hello)
+    callback = utils.getEventFunction(meta_data, payload)
+
+    return callback(meta_data, payload)
+
+
+def say_hello(meta_data: dict, payload: dict) -> dict:
+    # Print the "<helloKey> world!" string to the console.
+    result_text = utils.getSubmissionData(payload, "helloKey")[0] + " world!"
+    utils.setSubmissionData(payload, "resultKey", result_text)
+
     return payload
